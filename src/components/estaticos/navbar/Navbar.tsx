5bc8da8c0a06dc17d595d2edca8187041aa70270
Cipriana <ciprianaluis264@gmail.com>
2022-05-30 17:10:33 -0300

    Criando o componente de navegação

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+
+
+function Navbar() {
+    return (
+        <>
+            <AppBar position="static" className='back'>
+                <Toolbar variant="dense">
+                
+                <Link to='/login' className='text-decorator-none'> 
+                
+                    <Box justifyContent= "right" className= 'cursor'>
+                        <Typography variant="h5" color="inherit">
+                            
+                             <img src="https://i.imgur.com/7g7YSsV.png" width="150px" alt="" />
+                        </Typography>
+                    </Box>
+                     </Link>
+                    <Box display="flex" justifyContent="start">
+                    <Link to='/login' className='text-decorator-none'> 
+                        <Box mx={1}  className= 'cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Home
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link to='/postagens' className='text-decorator-none'> 
+                        <Box mx={1} className= 'cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Postagens
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link to='/temas' className='text-decorator-none'> 
+                        <Box mx={1} className= 'cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Temas 
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link to='/Cadastrar' className='text-decorator-none'> 
+                        <Box mx={1} className= 'cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Cadastrar tema
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link to='/login' className='text-decorator-none'> 
+                        <Box mx={1} className= 'cursor'>
+                        <Typography variant="h6" color="inherit">
+                                Login
+                            </Typography>
+                        </Box>
+                        </Link>
+                       
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar;
