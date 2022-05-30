16c5206e0f89c1ab91108de01fa9d323dae3ea5d
Patryck Silva <patrycksilva70@gmail.com>
2022-05-30 15:46:05 -0300

    Adicionando paginas home e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from './components/estaticos/footer/Footer'
-import Navbar from './components/estaticos/navbar/Navbar'
+
 import Home from './paginas/Home'
 import Login from './paginas/Login'
 import Sobre from './paginas/Sobre'
@@ -8,20 +8,20 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return(
+  return (
     <Router>
-    <Navbar />
-    <div style={{ minHeight: '100vh' }}>
-    <Routes> // Antigo Switch
-    <Route path="/" element={<Login />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/home" element={<Home />} />
-    <Route path="/sobre" element={<Sobre />} />
-    </Routes>
-    </div>
-    <Footer />
+      {/* <Navbar /> */}
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+        </Routes>
+      </div>
+      <Footer />
     </Router>
-    );
+  );
 }
 
 export default App;
