e6339eca5127295aab450e34d14ea4213e3f20fe
Cipriana <ciprianaluis264@gmail.com>
2022-06-02 15:05:46 -0300

    Modificando o componente Navbar

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -3,59 +3,55 @@ import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
 import {Link} from 'react-router-dom';
 import './Navbar.css';
 
-
 function Navbar() {
     return (
         <>
             <AppBar position="static" className='back'>
                 <Toolbar variant="dense">
-                
-                <Link to='/login' className='text-decorator-none'> 
-                
-                    <Box justifyContent= "right" className= 'cursor'>
+                <Link to='/blog' className='text-decorator-none'> 
+                    <Box className= 'cursor' >
                         <Typography variant="h5" color="inherit">
-                            
-                             <img src="https://i.imgur.com/7g7YSsV.png" width="150px" alt="" />
+                        <img src="https://i.imgur.com/7g7YSsV.png" width="150px" alt="" />
                         </Typography>
                     </Box>
-                     </Link>
+                    </Link>
+                    
                     <Box display="flex" justifyContent="start">
-                    <Link to='/login' className='text-decorator-none'> 
+                    <Link to='/home' className='text-decorator-none'> 
                         <Box mx={1}  className= 'cursor'>
                             <Typography variant="h6" color="inherit">
-                                Home
+                                home
                             </Typography>
                         </Box>
                         </Link>
-                        <Link to='/postagens' className='text-decorator-none'> 
+                        <Link to='/postagem' className='text-decorator-none'> 
                         <Box mx={1} className= 'cursor'>
                             <Typography variant="h6" color="inherit">
-                                Postagens
+                                postagens
                             </Typography>
                         </Box>
                         </Link>
                         <Link to='/temas' className='text-decorator-none'> 
                         <Box mx={1} className= 'cursor'>
                             <Typography variant="h6" color="inherit">
-                                Temas 
+                                temas 
                             </Typography>
                         </Box>
                         </Link>
-                        <Link to='/Cadastrar' className='text-decorator-none'> 
+                        <Link to='/cadastrar' className='text-decorator-none'> 
                         <Box mx={1} className= 'cursor'>
                             <Typography variant="h6" color="inherit">
-                                Cadastrar tema
+                                cadastrar tema
                             </Typography>
                         </Box>
                         </Link>
                         <Link to='/login' className='text-decorator-none'> 
                         <Box mx={1} className= 'cursor'>
                         <Typography variant="h6" color="inherit">
-                                Login
+                                logout
                             </Typography>
                         </Box>
                         </Link>
-                       
                     </Box>
 
                 </Toolbar>
