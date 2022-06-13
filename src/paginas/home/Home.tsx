bc4ef81ff4f0a690435c0ebea5abc7aef9063b31
JonathanBrasil <j_jonatha_2011@hotmail.com>
2022-06-13 20:10:30 -0300

    Estilizado pagina Home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -4,35 +4,33 @@ import './Home.css';
 import { Link } from 'react-router-dom';
 function Home() {
   return (
+
+
+
     <>
-      <Grid container className='gridmaior'>
-        <Grid item xs={6} className='grid1'>
-          <Box className='box1'>
-            <Box>
-              <Typography className='texto' gutterBottom >Ver postagens</Typography>
-              <Typography gutterBottom className='texto' >Criar postagem</Typography>
-              <Typography gutterBottom className='texto' >Criar tema</Typography>
-              <Link to='/login' className='text-decorator-none'><Typography gutterBottom className='texto' >Logar</Typography></Link>
-              <Typography gutterBottom className='texto' >Cadastrar</Typography>
-            </Box>
-          </Box>
 
-        </Grid>
-        <Grid item xs={6}>
-          <img src="https://i.imgur.com/7g7YSsV.png" alt=" logo graphome" width='700px' height='700px' />
-        </Grid>
-      </Grid>
-      <Grid container className='gridmaior'>
-        <Grid item xs={6} className='grid1'>
-          <Box className='box1'>
-            <Box>
-              <Typography gutterBottom className='texto' >Segundo grid é aqui para conteudo</Typography>
-            </Box>
-          </Box>
 
-        </Grid>
-        <Grid item xs={6}>
-          <Typography gutterBottom className='texto' >Segundo grid é aqui para conteudo</Typography>
+      <Grid className='homePrincipal '>
+        <Grid item xs={4} className="fundoHome"></Grid>
+
+        <Grid item xs={8}>
+
+          <div className="conteudo-direita">
+
+            <h1 className="titulo-Home">PINTOU MUDANÇA!</h1>
+
+            <h4 className="texto-Home">Uma rede social para compartilhar projetos de arquitetura colaborativo entre comunidade e profissionais, voltado para a reforma da paisagem urbana através de intervenção artística e gráfica com o objetivo de construir assentamentos urbanos mais agradáveis e seguros.
+              O intuito é fornecer um espaço voltado para organizar, construir e executar ideias e por fim, incentivar e inspirar pessoas com os resultados apresentados.</h4>
+            <div className='botao-Home'>
+              <Link to='/cadastro'>
+              <Button variant="outlined" className="botaoHome1"> FAZER CADASTRO </Button>
+              </Link>
+              <Link to='/login'>
+              <Button variant="outlined" className="botaoHome2"> LOGIN </Button>
+              </Link>
+            </div>
+          </div>
+
         </Grid>
       </Grid>
     </>
