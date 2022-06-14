888d16338e5489fc53e62a13f7562eb06308e37b
Patryck Silva <patrycksilva70@gmail.com>
2022-06-14 13:30:55 -0300

    Adicionando navbar pronto

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Toolbar, Typography, Box, InputBase, Button, Menu, MenuItem } from '@material-ui/core';
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
 import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,7 +13,61 @@ import { toast } from 'react-toastify';
 
 
 
+
 function Navbar() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    // criando styled components para searchbar
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    }));
+
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }));
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                width: '40ch',
+                '&:focus': {
+                    width: '48ch',
+                },
+            },
+        },
+    }));
+
+    // lógica de deslogar e só aparecer navbar se existir token
     let navigate = useNavigate()
 
     const token = useSelector<TokenState, TokenState["tokens"]>(
@@ -19,7 +77,7 @@ function Navbar() {
     let dispatch = useDispatch()
 
     function deslogar() {
-        dispatch(addToken(' '))
+        dispatch(addToken(''))
         toast.info('Usuário deslogado', {
             position: "top-right",
             autoClose: 3000,
@@ -35,9 +93,70 @@ function Navbar() {
     var navbarComponent
 
     if (token !== "") {
+
         navbarComponent =
-            <AppBar position="static" className='navbar'>
-                <Toolbar variant="dense">
+            <AppBar position="static" >
+                <Toolbar variant="dense" className='navbar'>
+                    <div className='divMenuIcon'>
+                        <Button id="basic-button" aria-controls={open ? 'basic-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} onClick={handleClick}>
+                            <IconButton size="large" edge="start" color="inherit" className='iconeMenu' aria-label="menu" sx={{ mr: 2 }}>
+                                <MenuIcon />
+                            </IconButton>
+                        </Button>
+                        <Menu id="basic-menu" anchorEl={anchorEl} open={open} onClose={handleClose} MenuListProps={{ 'aria-labelledby': 'basic-button', }} className='caixaMenu'>
+                            <MenuItem onClick={handleClose}> <Link to='/home' className='text-decorator-none'>
+                                <Box mx={1} className='cursor'>
+                                    <Typography variant="h6" color="inherit">
+                                        Home
+                                    </Typography>
+                                </Box>
+                            </Link></MenuItem>
+                            <MenuItem onClick={handleClose}> <Link to='/postagem' className='text-decorator-none'>
+                                <Box mx={1} className='cursor'>
+                                    <Typography variant="h6" color="inherit">
+                                        Postagens
+                                    </Typography>
+                                </Box>
+                            </Link></MenuItem>
+                            <MenuItem onClick={handleClose}><Link to='/temas' className='text-decorator-none'>
+                                <Box mx={1} className='cursor'>
+                                    <Typography variant="h6" color="inherit">
+                                        Categorias
+                                    </Typography>
+                                </Box>
+                            </Link></MenuItem>
+                            <MenuItem onClick={handleClose}><Link to='/feed' className='text-decorator-none'>
+                                <Box mx={1} className='cursor'>
+                                    <Typography variant="h6" color="inherit">
+                                        Feed
+                                    </Typography>
+                                </Box>
+                            </Link></MenuItem>
+                            <MenuItem onClick={handleClose}><Link to='/perfil' className='text-decorator-none'>
+                                <Box mx={1} className='cursor'>
+                                    <Typography variant="h6" color="inherit">
+                                        Perfil
+                                    </Typography>
+                                </Box>
+                            </Link></MenuItem>
+                            <MenuItem onClick={handleClose}>
+                                <Box mx={1} className='cursor' onClick={deslogar} >
+                                    <Typography variant="h6" color="inherit">
+                                        Logout
+                                    </Typography>
+                                </Box>
+                            </MenuItem>
+                        </Menu>
+                    </div>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
 
                     <Link to='/feed' className='text-decorator-none'>
                         <Box className='graphomeLogo' >
@@ -46,59 +165,7 @@ function Navbar() {
                             </Typography>
                         </Box>
                     </Link>
-                    <div className='botoesNavbar'>
-                        <Box display="flex" justifyContent="start">
-                            <Link to='/home' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        home
-                                    </Typography>
-                                </Box>
-                            </Link>
-                            <Link to='/postagem' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        postagens
-                                    </Typography>
-                                </Box>
-                            </Link>
-                            <Link to='/temas' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        temas
-                                    </Typography>
-                                </Box>
-                            </Link>
-                            <Link to='/formularioTema' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        cadastrar tema
-                                    </Typography>
-                                </Box>
-                            </Link>
-                            <Link to='/formulariopostagem' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        cadastrar postagem
-                                    </Typography>
-                                </Box>
-                            </Link>
-                            <Link to='/feed' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        feed
-                                    </Typography>
-                                </Box>
-                            </Link>
-                            <Link to='/login' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        logout
-                                    </Typography>
-                                </Box>
-                            </Link>
-                        </Box>
-                    </div>
+
                 </Toolbar>
             </AppBar>
     }
