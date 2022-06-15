929f4ada4daf0de17d1851087484d5be385daa96
Patryck Silva <patrycksilva70@gmail.com>
2022-06-15 11:33:45 -0300

    Estilizando botoes da aba de deletar temas

--- src/components/temas/deletartemas/DeletarTemas.tsx
+++ src/components/temas/deletartemas/DeletarTemas.tsx
@@ -97,12 +97,12 @@ function DeletarTemas() {
                     <CardActions>
                         <Box display="flex" justifyContent="start" ml={1.0} mb={2} >
                             <Box mx={2}>
-                                <Button onClick={sim} variant="contained" className="marginLeft" size='large' color="primary">
+                                <Button onClick={sim} variant="outlined" className="marginLeft" size='large' >
                                     Sim
                                 </Button>
                             </Box>
                             <Box mx={2}>
-                                <Button onClick={nao} variant="contained" size='large' color="secondary">
+                                <Button onClick={nao} variant="outlined" size='large' color="secondary">
                                     Não
                                 </Button>
                             </Box>
